681ecb5f4a6af501b5c75e3480201287fa34bff0
RubesWorld <hi@rubesworld.live>
2020-11-30 14:51:41 -0800

    almost done

--- js/index.js
+++ js/index.js
@@ -37,6 +37,60 @@ const siteContent = {
   },
 };
 
+
+//HEADER
+
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+//CTA
+//CTA - h1
+let ctaTitle = document.querySelector(".cta-text h1");
+ctaTitle.textContent = siteContent["cta"]["h1"];
+
+//CTA - Image   
+let bannerImage = document.querySelector("#cta-img");
+bannerImage.setAttribute('src',siteContent["cta"]["img-src"])
+
+//CTA - Button
+let ctaButton = document.querySelector(".cta-text button");
+ctaButton.textContent = siteContent["cta"]["button"];
+ctaButton.style.color = "black"; 
+
+//MAIN CONTENT
+//add features title
+let featuresTitle = document.querySelector(".top-content .text-content h4");
+featuresTitle.textContent = siteContent["main-content"]["features-h4"];
+
+//add features text 
+let featuresText = document.querySelector('.top-content .text-content p');
+featuresText.textContent = siteContent["main-content"]["features-content"]
+//add features title copy for About 
+let topContent = document.querySelector(".top-content .text-content");
+const topRightContent = topContent.cloneNode(true);
+document.querySelector(".top-content").appendChild(topRightContent);
+//replace class for second text-content
+topRightContent.classList.replace('text-content','text-content2');
+//update about title
+let aboutTitle = document.querySelector(".text-content2 h4");
+aboutTitle.textContent = siteContent["main-content"]["about-h4"];
+//update about text
+let aboutText = document.querySelector(".text-content2 p");
+aboutText.textContent = siteContent["main-content"]["about-content"];
+
+//add middle image 
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src',siteContent["main-content"]["middle-img-src"]);
+
+//bottom Content 
+
+let servicesTitle = document.querySelector(".bottom-content .text-content h4");
+servicesTitle.textContent = siteContent["main-content"]["services-h4"]
+
+let servicesText = document.querySelector('.bottom-content .text-content p');
+servicesText.textContent = siteContent["main-content"]["services-content"];
+
+
+// footer 
+let footerh4 = document.querySelector
